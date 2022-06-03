8cd4e4c251f9e5751c50f388f119098d26622d67
Admin <Admin@SENOL>
2022-06-03 11:46:41 +0300

    Add project files.

---
+++ GrayLogClientApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using Serilog;
+using Serilog.Sinks.Graylog;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("GrayLogs setting up ...");
+        GrayLogSetup();
+        Console.WriteLine("Graylogs settings is completed.");
+
+        Log.Error("Test log error message");
+        Log.Error("Test log error message template {TemplateName}", "CustomTemplate" );
+
+        try
+        {
+            int a = 0;
+            int b = 15 / a;
+            
+        }
+        catch (System.Exception ex)
+        {
+            Log.Error(ex, "An error occureted");
+        }
+
+        string stars = new string('*', 20);
+
+        Console.WriteLine($"\n\n{stars} Logs Completed! {stars}");
+
+        Console.ReadLine();
+    }
+
+    static void GrayLogSetup()
+    {
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Graylog(new GraylogSinkOptions
+            {
+                Facility = "GrapLogClientApp",
+                SerializerSettings = new Newtonsoft.Json.JsonSerializerSettings
+                {
+                    DateFormatString = "yyyy-MM-dd"
+                },
+                HostnameOrAddress = "localhost",
+                Port = 12201,
+                TransportType = Serilog.Sinks.Graylog.Core.Transport.TransportType.Udp
+            })
+            .CreateLogger();
+    }
+}
